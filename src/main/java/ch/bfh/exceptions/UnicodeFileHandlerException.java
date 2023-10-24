a5ca54a2cee9b6949e9ecb1a8a93e5aac27fbd31
wampk1 <kilian.wampfler@students.bfh.ch>
2023-10-24 19:47:23 +0200

    renamed Exception

--- src/main/java/ch/bfh/exceptions/UnicodeFileHandlerException.java
+++ src/main/java/ch/bfh/exceptions/UnicodeFileHandlerException.java
@@ -4,13 +4,13 @@ package ch.bfh.exceptions;
  * Custom exception to handle invalid unicode file formats cases
  */
 
-public class UnicodeFileFormatException extends Exception {
+public class UnicodeFileHandlerException extends Exception {
      /**
      * Constructs a new exception with the specified detail message.
      *
      * @param message the detail message
      */
-    public UnicodeFileFormatException(String message) {
+    public UnicodeFileHandlerException(String message) {
         super(message);
     }
 }
