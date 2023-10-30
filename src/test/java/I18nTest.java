5e8946f490236b4ec24096bbd28dcc834a8e7024
Nicolin Dora <mail@nicolin-dora.ch>
2023-10-30 13:05:56 +0100

    Adds testcase for the I18n Class

---
+++ src/test/java/I18nTest.java
@@ -0,0 +1,85 @@
+import ch.bfh.helper.I18n;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the {@link I18n} helper class.
+ */
+class I18nTest {
+
+    /**
+     * Setup logic executed before each test. Ensures fresh loading
+     * of the ResourceBundle for each test case.
+     */
+    @BeforeEach
+    void setup() {
+        // Reset the ResourceBundle to null before each test to ensure
+        // fresh loading of the bundle for each test case.
+        I18n.getResourceBundle(Locale.ENGLISH);
+    }
+
+    /**
+     * Test method for {@link I18n#getResourceBundle(Locale)}.
+     * Validates that the correct resource bundle is loaded for a given locale.
+     */
+    @Test
+    void testGetResourceBundle() {
+        // Testing for English locale
+        Locale locale = Locale.ENGLISH;
+        assertEquals(locale, I18n.getResourceBundle(locale).getLocale());
+
+        // Testing for more languages
+        // locale = Locale.FRANCE;
+        // assertEquals(locale, I18n.getResourceBundle(locale).getLocale());
+    }
+
+    /**
+     * Test method for {@link I18n#getString(String)}.
+     * Validates the value returned for a valid key.
+     */
+    @Test
+    void testGetString_ValidKey() {
+        String key = "path.prompt";
+        String expectedValue = "Enter the file/directory path:";
+        assertEquals(expectedValue, I18n.getString(key));
+    }
+
+    /**
+     * Test method for {@link I18n#getString(String)}.
+     * Validates the value returned when an invalid key is provided.
+     */
+    @Test
+    void testGetString_InvalidKey() {
+        String key = "invalidKey";
+        String expectedValue = "!" + key + "!";
+        assertEquals(expectedValue, I18n.getString(key));
+    }
+
+    /**
+     * Test method for {@link I18n#getString(String, Object...)}.
+     * Validates the formatted value returned for a valid key with parameters.
+     */
+    @Test
+    void testGetStringWithParams_ValidKey() {
+        String key = "action.archiving"; // Assuming "greeting" exists in messages.properties with value "Hello, {0}!"
+        String param = "test_url.ch";
+        String expectedValue = "Archiving test_url.ch ...";
+        assertEquals(expectedValue, I18n.getString(key, param));
+    }
+
+    /**
+     * Test method for {@link I18n#getString(String, Object...)}.
+     * Validates the value returned when an invalid key with parameters is provided.
+     */
+    @Test
+    void testGetStringWithParams_InvalidKey() {
+        String key = "invalidGreeting";
+        String param = "Alice";
+        String expectedValue = "!" + key + "!";
+        assertEquals(expectedValue, I18n.getString(key, param));
+    }
+}
