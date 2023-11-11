94b0a92e89061305817c97d49aae7a87c488a565
Nicolin Dora <mail@nicolin-dora.ch>
2023-11-11 16:50:05 +0100

    Style: Minor refactoring
    Adds some comments and other minor changes

--- src/main/java/ch/bfh/model/FileModel.java
+++ src/main/java/ch/bfh/model/FileModel.java
@@ -1,33 +1,27 @@
 package ch.bfh.model;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
-
-import org.apache.pdfbox.Loader;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
+import java.util.Set;
 
 /**
- * Handler for validating and converting a file to a string for processing
+ * Represents a file for URL processing, holding metadata and content-related URLs.
  */
 public class FileModel {
 
-    private String mimeType;
-    private String fileName;
-    private String stringPath;
+    private final String mimeType;
+    private final String fileName;
+    private final Path filePath;
     private List<URLPair> urlPairs;
 
 
     /**
-     * Constructor for fileModel object
-     * @param inputPath needs as input a path to a file
-     * @param mimeType needs as input the type of the file (only plaintext and pdf are supported)
+     * Constructs a FileModel with a path and MIME type.
+     *
+     * @param filePath the file's path
+     * @param mimeType the file's MIME type
      */
     public FileModel(String inputPath, String mimeType) {
         this.mimeType = mimeType;
@@ -38,9 +32,9 @@ public class FileModel {
     }
 
     /**
-     * method which converts a unicode file or pdf file to a string (not for files larger than 2GB)
-     * @return returns the file as a string object
-     * @throws IOException
+     * Returns the file name.
+     *
+     * @return the file name as a string
      */
     public String fileToString() throws IOException {
         if (this.mimeType.equals("application/pdf")) {
@@ -65,38 +59,50 @@ public class FileModel {
     }
 
     /**
-     * Sets the list of URL pairs.
+     * Returns the MIME type of the file.
      *
-     * @param urlPairs the list of URL pairs to set
+     * @return the MIME type as a string
      */
     public void setUrlPairs(List<URLPair> urlPairs) {
         this.urlPairs = urlPairs;
     }
 
     /**
-     * Retrieves the list of URL pairs.
+     * Returns the file path.
      *
-     * @return the list of URL pairs
+     * @return the file path as a Path object
+     */
+    public Path getFilePath() {
+        return filePath;
+    }
+
+
+    /**
+     * Returns the list of URL pairs.
+     *
+     * @return an unmodifiable list of URLPair objects
      */
     public List<URLPair> getUrlPairs() {
-        return urlPairs;
+        if(this.urlPairs == null){
+            return Collections.emptyList();
+        }
+        return this.urlPairs;
     }
 
     /**
-     * Adds an extracted URL with its line number to the list.
+     * Adds a list of extracted URLs to the URL pairs.
      *
-     * @param extractedURL the extracted URL to add
-     * @param lineNumber   the line number where the URL was extracted from
+     * @param extractedURLs a list of URLs as strings
      */
     public void addExtractedURL(String extractedURL, int lineNumber) {
         urlPairs.add(new URLPair(extractedURL, lineNumber));
     }
 
     /**
-     * Sets the archived URL corresponding to the provided extracted URL.
+     * Associates an archived URL with its corresponding extracted URL in the list of URL pairs.
      *
-     * @param extractedURL the extracted URL for which the archived URL is to be set
-     * @param archivedURL  the archived URL to be associated with the extracted URL
+     * @param extractedURL the URL that was extracted from the file content
+     * @param archivedURL  the URL that was archived
      */
     public void setArchivedURL(String extractedURL, String archivedURL) {
         for (URLPair pair : urlPairs) {
