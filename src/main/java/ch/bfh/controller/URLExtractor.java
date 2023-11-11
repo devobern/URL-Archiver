82e30e8d0e6df5cc0a46f994be46e6cacc2010b9
Vejsa1 <vejsa1@bfh.ch>
2023-11-12 00:06:21 +0100

    Doc: Add doc comments

--- src/main/java/ch/bfh/controller/URLExtractor.java
+++ src/main/java/ch/bfh/controller/URLExtractor.java
@@ -5,9 +5,22 @@ import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Provides utility for extracting URLs from a given text string.
+ * This class uses a regular expression to identify and extract URLs, focusing primarily on HTTP and HTTPS formats.
+ * It is capable of handling multiple URLs within a single text string and ensures uniqueness of the extracted URLs.
+ */
 public class URLExtractor {
     public URLExtractor() {
     }
+
+    /**
+     * Extracts a set of unique URLs from the provided text string.
+     * The method uses a regular expression to match HTTP and HTTPS URLs within the text and collects them into a set to ensure uniqueness.
+     *
+     * @param text The string from which URLs are to be extracted.
+     * @return A Set of unique URLs found in the provided text. If no URLs are found, an empty Set is returned.
+     */
     public static Set<String> extractURLs(String text) {
         Set<String> urls = new HashSet<>();
         // Regular expression for URL
