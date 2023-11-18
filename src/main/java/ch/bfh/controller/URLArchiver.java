c125b5c71c5c0f3adeff0013bae8f7b8feb2b689
Nicolin Dora <mail@nicolin-dora.ch>
2023-11-18 11:14:03 +0100

    Del: Delete unused class URLArchiver

--- src/main/java/ch/bfh/controller/URLArchiver.java
+++
@@ -1,8 +0,0 @@
-package ch.bfh.controller;
-
-public class URLArchiver {
-    public static String archiveURL(String url){
-        // todo: archiving logic
-        return "example.archive.com";
-    }
-}
