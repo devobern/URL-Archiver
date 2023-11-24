dc3825f608c7528e3fb02b688c2dea4fefc01e7b
wampk1 <kilian.wampfler@students.bfh.ch>
2023-11-24 16:19:38 +0100

    add: Dataclass WaybackMAchineJob
    add dataclass for Objectmapping the response of get archive job request

---
+++ src/main/java/ch/bfh/model/WaybackMachineJob.java
@@ -0,0 +1,43 @@
+package ch.bfh.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WaybackMachineJob {
+    private String job_id;
+    private String original_url;
+    private String status;
+    private String timestamp;
+
+    public String getJob_id() {
+        return job_id;
+    }
+
+    public void setJob_id(String job_id) {
+        this.job_id = job_id;
+    }
+
+    public String getOriginal_url() {
+        return original_url;
+    }
+
+    public void setOriginal_url(String original_url) {
+        this.original_url = original_url;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
