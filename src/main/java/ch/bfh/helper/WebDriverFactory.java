6f55c7cc5c52511a6f6d65dcec95b1eba65f3ec5
Vejsa1 <vejsa1@bfh.ch>
2023-11-26 21:27:26 +0100

    Fix: Add Javadoc

--- src/main/java/ch/bfh/helper/WebDriverFactory.java
+++ src/main/java/ch/bfh/helper/WebDriverFactory.java
@@ -11,31 +11,64 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 
+/**
+ * A factory class for creating instances of {@link WebDriver} for different browsers.
+ * This class provides a centralized way to create and configure web drivers for
+ * Edge, Safari, Firefox, and Chrome. Based on the operating system an appropriate
+ * WebDriver instance with a common page load strategy is returned.
+ * Windows, macOS, and Linux are supported operating systems.
+ */
 public class WebDriverFactory {
 
+    /**
+     * Creates and returns EdgeOptions with the EAGER {@link PageLoadStrategy}.
+     *
+     * @return {@link EdgeOptions} configured for the WebDriver.
+     */
     private static EdgeOptions getEdgeOptions() {
         EdgeOptions options = new EdgeOptions();
         options.setPageLoadStrategy(PageLoadStrategy.EAGER);
         return options;
     }
 
+    /**
+     * Creates and returns SafariOptions with the EAGER {@link PageLoadStrategy}.
+     *
+     * @return {@link SafariOptions} configured for the WebDriver.
+     */
     private static SafariOptions getSafariOptions() {
         SafariOptions options = new SafariOptions();
         options.setPageLoadStrategy(PageLoadStrategy.EAGER);
         return options;
     }
 
+    /**
+     * Creates and returns ChromeOptions with the EAGER {@link PageLoadStrategy}.
+     *
+     * @return {@link ChromeOptions} configured for the WebDriver.
+     */
     private static ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
         options.setPageLoadStrategy(PageLoadStrategy.EAGER);
         return options;
     }
 
+    /**
+     * Creates and returns FirefoxOptions with the EAGER {@link PageLoadStrategy}.
+     *
+     * @return {@link FirefoxOptions} configured for the WebDriver.
+     */
     private static FirefoxOptions getFirefoxOptions() {
         FirefoxOptions options = new FirefoxOptions();
         options.setPageLoadStrategy(PageLoadStrategy.EAGER);
         return options;
     }
+
+    /**
+     * Determines the operating system of the current environment.
+     *
+     * @return A string representing the operating system name, e.g., "Windows", "macOS", "Linux".
+     */
     private static String getOperatingSystem() {
         String os = System.getProperty("os.name").toLowerCase();
         return switch (os) {
@@ -46,6 +79,13 @@ public class WebDriverFactory {
         };
     }
 
+    /**
+     * Provides a WebDriver instance based on the current operating system.
+     * It returns an EdgeDriver for Windows, SafariDriver for macOS, FirefoxDriver for Linux,
+     * and ChromeDriver for other or unknown operating systems.
+     *
+     * @return An instance of {@link WebDriver} appropriate for the current operating system.
+     */
     public static WebDriver getWebDriver() {
         String currentOS = getOperatingSystem();
         return switch (currentOS) {
