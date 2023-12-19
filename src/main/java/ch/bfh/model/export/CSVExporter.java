6badcc7c4a86f1fa8073f79a8b13904520857cf9
Nicolin Dora <mail@nicolin-dora.ch>
2023-12-19 18:26:38 +0100

    Feat: Template for CSV Export

---
+++ src/main/java/ch/bfh/model/export/CSVExporter.java
@@ -0,0 +1,13 @@
+package ch.bfh.model.export;
+
+import ch.bfh.model.FileModel;
+
+import java.io.IOException;
+
+public class CSVExporter implements Exporter {
+
+    @Override
+    public void exportURLs(FileModel fileModel, String destinationPath) throws IOException {
+
+    }
+}
