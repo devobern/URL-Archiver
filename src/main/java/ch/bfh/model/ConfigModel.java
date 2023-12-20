8342be1574160f4e9aaad5c1e4f6247823bfbef8
Vejsa1 <vejsa1@bfh.ch>
2023-12-20 14:07:43 +0100

    Fix: Add JavaDoc

--- src/main/java/ch/bfh/model/ConfigModel.java
+++ src/main/java/ch/bfh/model/ConfigModel.java
@@ -1,21 +1,43 @@
 package ch.bfh.model;
 
+/**
+ * Represents the configuration settings for the application.
+ * This includes access key, secret key, and browser preference.
+ */
 public class ConfigModel {
     private String accessKey;
     private String secretKey;
     private SupportedBrowsers browser;
 
+    /**
+     * Default constructor initializing with default values.
+     */
     public ConfigModel() {
         this.accessKey = "";
         this.secretKey = "";
         this.browser = SupportedBrowsers.DEFAULT;
     }
+
+    /**
+     * Constructor to initialize with specific access and secret keys.
+     * The browser is set to the default.
+     *
+     * @param accessKey The access key to be used.
+     * @param secretKey The secret key to be used.
+     */
     public ConfigModel(String accessKey, String secretKey) {
         this.accessKey = accessKey;
         this.secretKey = secretKey;
         this.browser = SupportedBrowsers.DEFAULT;
     }
 
+    /**
+     * Constructor to initialize with specific access key, secret key, and browser type.
+     *
+     * @param accessKey The access key to be used.
+     * @param secretKey The secret key to be used.
+     * @param browser   The browser preference.
+     */
     public ConfigModel(String accessKey, String secretKey, SupportedBrowsers browser) {
         this.accessKey = accessKey;
         this.secretKey = secretKey;
@@ -34,7 +56,7 @@ public class ConfigModel {
         return secretKey;
     }
 
-    public void setSecretKey(String secretKey) {
+     public void setSecretKey(String secretKey) {
         this.secretKey = secretKey;
     }
 
