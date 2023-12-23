c9da6a639cae953d8e82b1a063663d2e88d42516
Vejsa1 <vejsa1@bfh.ch>
2023-12-23 16:44:49 +0100

    Doc: Add JavaDoc

--- src/test/java/ch/bfh/helper/BrowserOpenerTest.java
+++ src/test/java/ch/bfh/helper/BrowserOpenerTest.java
@@ -9,14 +9,24 @@ import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for the {@link BrowserOpener} class.
+ */
 public class BrowserOpenerTest {
 
+    /**
+     * Verifies that no exceptions are thrown when a valid URL is provided for opening in the browser.
+     */
     @Test
     void openURLWithValidURL() {
         // It's hard to verify browser opening, but no exceptions should be thrown for a valid URL
         assertDoesNotThrow(() -> BrowserOpener.openURL("http://example.com"));
     }
 
+    /**
+     * Tests that an {@link UnsupportedOperationException} is thrown when the desktop environment
+     * does not support browsing URLs.
+     */
     @Test
     void openURLWhenDesktopNotSupported() {
         try (MockedStatic<Desktop> mockedDesktop = Mockito.mockStatic(Desktop.class)) {
@@ -27,6 +37,10 @@ public class BrowserOpenerTest {
         }
     }
 
+    /**
+     * Tests that an {@link UnsupportedOperationException} is thrown when the browse action is not
+     * supported in the desktop environment, even if desktop is supported.
+     */
     @Test
     void openURLWhenBrowseActionNotSupported() {
         try (MockedStatic<Desktop> mockedDesktop = Mockito.mockStatic(Desktop.class)) {
