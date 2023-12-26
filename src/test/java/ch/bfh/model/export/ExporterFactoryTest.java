226e275ed4de05d67e40175db3e16da0502e360e
Vejsa1 <vejsa1@bfh.ch>
2023-12-26 03:33:29 +0100

    Feat: Add JUnit tests with doc

---
+++ src/test/java/ch/bfh/model/export/ExporterFactoryTest.java
@@ -0,0 +1,35 @@
+package ch.bfh.model.export;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExporterFactoryTest {
+
+    /**
+     * Test that getExporter returns a CSVExporter instance when "csv" type is provided.
+     */
+    @Test
+    void testGetCsvExporter() {
+        Exporter exporter = ExporterFactory.getExporter("csv");
+        assertTrue(exporter instanceof CSVExporter, "getExporter should return a CSVExporter for 'csv' type");
+    }
+
+    /**
+     * Test that getExporter returns a BIBExporter instance when "bib" type is provided.
+     */
+    @Test
+    void testGetBibExporter() {
+        Exporter exporter = ExporterFactory.getExporter("bib");
+        assertTrue(exporter instanceof BIBExporter, "getExporter should return a BIBExporter for 'bib' type");
+    }
+
+    /**
+     * Test that getExporter throws an IllegalArgumentException for an unknown exporter type.
+     */
+    @Test
+    void testGetUnknownExporter() {
+        assertThrows(IllegalArgumentException.class, () -> ExporterFactory.getExporter("unknown"),
+                "getExporter should throw an IllegalArgumentException for unknown types");
+    }
+}
