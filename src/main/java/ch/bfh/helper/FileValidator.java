80de5339a28d57a1571185a0b3a0aea94b7b2dde
Nicolin Dora <mail@nicolin-dora.ch>
2023-12-28 17:03:17 +0100

    Fix: Fix bug, where .bib files are not recognized on Windows os

--- src/main/java/ch/bfh/helper/FileValidator.java
+++ src/main/java/ch/bfh/helper/FileValidator.java
@@ -32,10 +32,16 @@ public class FileValidator {
         }
 
         String mimeType = Files.probeContentType(path);
+        System.out.println("MIME-Type: " + mimeType);
         if (mimeType != null) {
             if (mimeType.equals("application/pdf") || mimeType.startsWith("text/")) {
                 return mimeType;
             }
+        }
+
+        // Check if the file is a .bib file. This step is necessary only on Windows, as MimeTypes are handled differently on windows.
+        if (getFileExtension(stringPath).equals("bib")) {
+            return "text/bib";
         }
 
         try {
@@ -49,6 +55,13 @@ public class FileValidator {
         throw new FileModelException(I18n.getString("folder.skipFile.info") + path.getFileName().toString() + " (" + mimeType + ")");
     }
 
+    /**
+     * Checks if the given file path refers to a plain text file.
+     *
+     * @param path The path of the file to be checked.
+     * @return {@code true} if the file is a plain text file, {@code false} otherwise.
+     * @throws IOException If an I/O error occurs, such as if the file is empty or cannot be read.
+     */
     private static boolean isPlainTextFile(Path path) throws IOException {
         try (InputStream in = Files.newInputStream(path)) {
             byte[] buffer = new byte[1024];
@@ -61,7 +74,14 @@ public class FileValidator {
         }
     }
 
-
+    /**
+     * Determines if the provided byte array represents text.
+     * This method attempts to decode the array using UTF-8 encoding. If decoding is successful and the result is non-empty, the data is considered to be text.
+     *
+     * @param data   The byte array to check.
+     * @param length The number of bytes to use from the array.
+     * @return {@code true} if the specified portion of the array represents text, {@code false} otherwise.
+     */
     private static boolean isText(byte[] data, int length) {
         if (length <= 0) {
             return false;
@@ -73,6 +93,28 @@ public class FileValidator {
             return !StandardCharsets.UTF_8.newDecoder().decode(buffer).toString().isEmpty();
         } catch (CharacterCodingException e) {
             return false;
+        }
+    }
+
+    /**
+     * Extracts the file extension from a file path.
+     * This method considers the part of the string after the last period ('.') as the file extension. If there is no period or the last period is part of a directory name, an empty string is returned.
+     *
+     * @param filePath The file path from which to extract the extension.
+     * @return The file extension, or an empty string if no extension is found.
+     */
+    private static String getFileExtension(String filePath) {
+        // Find the last index of a period
+        int lastIndexOfPeriod = filePath.lastIndexOf(".");
+
+        // Find the last index of a file separator
+        int lastIndexOfSeparator = Math.max(filePath.lastIndexOf("/"), filePath.lastIndexOf("\\"));
+
+        // Check if the last period is part of the filename (not a directory)
+        if (lastIndexOfPeriod > lastIndexOfSeparator) {
+            return filePath.substring(lastIndexOfPeriod + 1);
+        } else {
+            return ""; // No extension found
         }
     }
 }
