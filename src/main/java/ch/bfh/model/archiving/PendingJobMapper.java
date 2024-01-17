231fcd4258d44aa110780b3d3a64909abc682d40
Vejsa1 <vejsa1@bfh.ch>
2024-01-17 22:41:20 +0100

    Doc: Add JavaDoc and reformat

--- src/main/java/ch/bfh/model/archiving/PendingJobMapper.java
+++ src/main/java/ch/bfh/model/archiving/PendingJobMapper.java
@@ -1,14 +1,26 @@
 package ch.bfh.model.archiving;
 
+/**
+ * Represents a mapping for a pending job in the archiving process, associating an extracted URL with a job ID.
+ */
 public class PendingJobMapper {
     private String extractedUrl;
     private String job_id;
 
+    /**
+     * Constructs a new PendingJobMapper with specified URL and job ID.
+     *
+     * @param extractedUrl The extracted URL to be associated with the job.
+     * @param job_id       The unique identifier of the job.
+     */
     public PendingJobMapper(String extractedUrl, String job_id) {
         this.extractedUrl = extractedUrl;
         this.job_id = job_id;
     }
 
+    /**
+     * Default constructor initializing fields with empty strings.
+     */
     public PendingJobMapper() {
         this.extractedUrl = "";
         this.job_id = "";
